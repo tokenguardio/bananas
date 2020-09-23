a53416116b7d8d152a6beb043a375246aeb83a3e
Piotr Piwoński <peter@blockhunters.io>
2020-09-23 14:39:43 +0200

    Add symbolics

--- bananas/ast/branchmonkey/variables.py
+++ bananas/ast/branchmonkey/variables.py
@@ -1,8 +1,10 @@
 from dataclasses import dataclass
-from typing import Tuple
+from typing import Tuple, Union
 
 from bananas.ast.common import quote
+from bananas.ast.webassembly.datatypes import TypeSubOp
 from bananas.serializer.node import Node, to_sexpr
+from bananas.serializer.stringify import sexpr_to_str
 
 
 @dataclass
@@ -30,3 +32,33 @@ class Argv(Node):
     @staticmethod
     def create(*args):
         return Argv(args)
+
+
+@dataclass
+class Symbol(Node):
+    op = "symbol"
+    name: str
+    value: Union[str, TypeSubOp]
+
+    def to_sexpr(self):
+        if isinstance(self.value, TypeSubOp):
+            value = self.value.to_sexpr()
+        else:
+            value = quote(self.value)
+        return self.op, quote(self.name), value
+
+
+@dataclass
+class Symbolics(Node):
+    op = "symbolics"
+    symbols: Tuple[Symbol]
+
+    def to_sexpr(self):
+        return self.op, *map(to_sexpr, self.symbols)
+
+    def to_string(self):
+        return f";; {super().to_string()}"
+
+    @staticmethod
+    def create(*symbols):
+        return Symbolics(symbols)
